8db1a5bc8a075153658512a6d62b8908e27ff513
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2024-03-04 22:17:08 +0800

    chore(animations): added animation for about us

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -2,3 +2,21 @@ function shrinkImage() {
     var imageContainer = document.querySelector('.shrink-container');
     imageContainer.classList.toggle('shrink');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const descriptionBoxes = document.querySelectorAll('.description-box');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+            } else {
+                entry.target.classList.remove('fade-in');
+            }
+        });
+    }, { threshold: 0.5 });
+
+    descriptionBoxes.forEach(box => {
+        observer.observe(box);
+    });
+});
