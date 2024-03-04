8ff8f861664d5ebcd01f5272cb92a125e204ac27
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2024-03-04 22:55:09 +0800

    chore(animations): added services animation

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,10 +1,13 @@
 function shrinkImage() {
+    // 
+    // Hero Section
+    // 
+
     var imageContainer = document.querySelector('.shrink-container');
     imageContainer.classList.toggle('shrink');
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    const descriptionBoxes = document.querySelectorAll('.description-box');
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
@@ -16,7 +19,41 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }, { threshold: 0.5 });
 
+    //
+    // About section
+    // 
+
+    const descriptionBoxes = document.querySelectorAll('.description-box');
+
     descriptionBoxes.forEach(box => {
         observer.observe(box);
     });
+
+    //
+    // Services Section
+    // 
+
+    const servicesBoxes = document.querySelectorAll('.services-box');
+
+    servicesBoxes.forEach(box => {
+        observer.observe(box);
+    });
+
+    var iconContainers = document.querySelectorAll('.services-icon');
+
+    function handleScroll() {
+        iconContainers.forEach(function (container) {
+            var rect = container.getBoundingClientRect();
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                container.classList.add('big');
+            } else {
+                container.classList.remove('big');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
 });
+
